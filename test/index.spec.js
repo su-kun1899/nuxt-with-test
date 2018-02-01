801e019826a280a599da145dc75ebf5290eeda9a
su-kun1899 <higedrum.coz@gmail.com>
2018-02-01 12:19:11 +0900

    AVAによるテストを追加

---
+++ test/index.spec.js
@@ -0,0 +1,43 @@
+import test from 'ava'
+import { Nuxt, Builder } from 'nuxt'
+import { resolve } from 'path'
+
+// Nuxt への参照を保持します
+// そうすればテスト終了時にサーバーをクローズできます
+let nuxt = null
+
+// Nuxt.js を初期化し localhost:4000 のリスニングを開始します
+test.before('Init Nuxt.js', async t => {
+  const rootDir = resolve(__dirname, '..')
+  let config = {}
+  try {
+    config = require(resolve(rootDir, 'nuxt.config.js'))
+  } catch (e) {}
+  config.rootDir = rootDir // project folder
+  config.dev = false // production build
+  nuxt = new Nuxt(config)
+  await new Builder(nuxt).build()
+  nuxt.listen(4000, 'localhost')
+})
+
+// 生成された HTML のみをテストする例
+test('Route / exits and render HTML', async t => {
+  let context = {}
+  const { html } = await nuxt.renderRoute('/ava', context)
+  t.true(html.includes('<h1 class="red">Hello AVA!</h1>'))
+})
+
+// DOM チェックを経由してテストする例
+test('Route / exits and render HTML with CSS applied', async t => {
+  const window = await nuxt.renderAndGetWindow('http://localhost:4000/ava')
+  const element = window.document.querySelector('.red')
+  t.not(element, null)
+  t.is(element.textContent, 'Hello AVA!')
+  t.is(element.className, 'red')
+  t.is(window.getComputedStyle(element).color, 'red')
+})
+
+// Nuxt サーバーをクローズする
+test.after('Closing server', t => {
+  nuxt.close()
+})
